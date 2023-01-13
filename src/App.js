0babe2f1561d33c803a67e67f2d9ecb7c1a07b6b
권도은 <kounduk9119t@naver.com>
2023-01-13 18:13:05 +0900

    "2023-01-13"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import MyComponent3 from "./compoenet/MuComponent3";
+import MyComponent from "./compoenet/MyComponent";
+import MyComponent2 from "./compoenet/MyComponent2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    return (
+        <Fragment>
+            <h3>나의 App.JS</h3>
+            {/* props */}
+            <MyComponent name={"홍길동"} age={20} />
+            <MyComponent2/>
+            <MyComponent3/>
+        </Fragment>
+    )
 }
 
-export default App;
+
+export default App; //내보내기
---
+++ src/compoenet/MuComponent3.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+
+class MyComponent3 extends Component {
+
+    //클래스형 컴포넌트 render함수 안에서 return문을 작성
+    render() { //컴포넌트가 제공
+        return (
+            <div>나의 클래스형 컴포넌트</div>
+        )
+    }
+}
+
+export default MyComponent3;
